4e69ab1d868d8fde24645be7859b93f154462d94
thebelovedmoon <103877195+thebelovedmoon@users.noreply.github.com>
2023-12-31 22:53:01 +0800

    revealed: Mangas of 2023

--- sumiyoi/yearinmanga/scripts.js
+++ sumiyoi/yearinmanga/scripts.js
@@ -22,6 +22,7 @@ function startView() {
     document.getElementById("disappearIntro").style.display = "none";
     document.documentElement.style.pointerEvents = "all";
     document.documentElement.style.cursor = "auto";
+    document.getElementById("disappearIntro").remove();
     window.onscroll = function() {showBackToTopBtn()};
   }, 750);
 }
@@ -38,3 +39,25 @@ function backToTop() {
   document.documentElement.scrollTop = 0;
 }
 // code: https://w3schools.com/howto/tryit.asp?filename=tryhow_js_scroll_to_top
+
+function revealEmbed(ytValue) {
+  document.getElementById("showEmbed").style.display = "block";
+  document.getElementById("embedViewer").innerHTML = `
+    <iframe
+      id="manipulateYTVideo"
+      src="https://www.youtube-nocookie.com/embed/` + ytValue + `"
+      allowfullscreen>
+    </iframe>
+  `;
+}
+// code: https://stackoverflow.com/questions/53757835/get-different-values-with-same-id-in-javascript
+function disappearModal() {
+  ytValue = null;
+  let stopVideo = document.querySelector("iframe");
+  if (stopVideo) {
+    let embedConfuse = stopVideo.src;
+    stopVideo.src = embedConfuse;
+  }
+  document.getElementById("showEmbed").style.display = "none";
+}
+// simplified from https://makarov1011.medium.com/stop-videos-embedded-in-your-html-script-using-js-9eda5113f873
