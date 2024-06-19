4e9948f71d6c1bbf8a1104d5f8b09e47afc31da6
thebelovedmoon <103877195+thebelovedmoon@users.noreply.github.com>
2024-06-19 14:44:54 +0800

    BIG MIGRATION INCOMINGGGG

--- sumiyoi/youjokitan/init.js
+++
@@ -1,6 +0,0 @@
-function sayonara() {
-  //window.location = "sayonara.html";
-  window.location = "letter.html";
-}
-
-setTimeout('sayonara()', 30000);
--- sumiyoi/youjokitan/scripts.js
+++
@@ -1,6 +0,0 @@
-function Load() {
-  setTimeout(function() {
-    document.getElementById("loader").remove();
-    document.getElementById("loadBlock").style.height = "0";
-  }, 1000);
-}
