9730ed478f5644f50245b2fd002d234c38bbf0c3
thebelovedmoon <103877195+thebelovedmoon@users.noreply.github.com>
2024-10-28 12:48:31 +0800

    [🍃] url change

--- assets/js/nav_firstMaria.js
+++ assets/js/nav_firstMaria.js
@@ -38,7 +38,7 @@ function webQuery() {
     for (let m = 1; m < 3; m++) {
       document.getElementById("miscBtn" + m).innerHTML = "open_in_new";
       document.getElementById("miscBtn" + m).title = "learn more";
-      document.getElementById("miscBtn" + m).onclick = () => { window.open("https://agasitecollection.github.io/cosmariainception"); };
+      document.getElementById("miscBtn" + m).onclick = () => { window.open("https://tbmassoc.github.io/thebelovedmoon/cosmariainauguration"); };
       document.getElementById("miscBtn" + m).style.display = "inline-block";
     }
 
